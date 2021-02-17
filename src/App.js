86583b1d50b85dc74fad39dd86c782c8c0428997
georgejeswin <jeswinmyladoor@gmail.com>
2021-02-17 23:43:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./App.css";
+import ReactDOM from "react-dom";
+
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
+function App() {
+  const [price, setPrice] = useState(0);
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: price,
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture();
+  };
+
+  return (
+    <div className="app">
+      <div className="wrapper">
+        <div className="inputContainer">
+          <label>Enter the amount</label>
+          <input
+            type="number"
+            value={price}
+            placeholder="Enter the amount"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+
+        <PayPalButton
+          createOrder={(data, actions) => createOrder(data, actions)}
+          onApprove={(data, actions) => onApprove(data, actions)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
